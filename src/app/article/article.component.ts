6937734c1d10ed8fc6b3e1a7cf76f5fa0a1237fe
Mahery Gonin <mahery.gonin@gmail.com>
2024-09-25 16:02:33 +0200

    add interface for Article

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,6 +1,15 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+interface Article {
+  title: string;
+  author: string;
+  content: string;
+  image: string;
+  isPublished: boolean;
+  comment?: string;
+}
+
 @Component({
   selector: 'app-article',
   standalone: true,
@@ -8,8 +17,9 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './article.component.html',
   styleUrl: './article.component.scss'
 })
+
 export class ArticleComponent {
-  article = {
+  article: Article = {
     title: 'Titre de l\'article',
     author: 'John Doe',
     content: 'Voici le contenu de l\'article.',
