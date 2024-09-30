0db94ab06e2ec46167ee037d65fca6ce0618451e
Mahery Gonin <mahery.gonin@gmail.com>
2024-09-30 16:03:04 +0200

    commit

---
+++ src/app/contact-form/contact-form.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Message } from '../models/message.model';
+
+@Component({
+  selector: 'app-contact-form',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './contact-form.component.html',
+  styleUrl: './contact-form.component.scss'
+})
+export class ContactFormComponent {
+
+  newMessage: Message = {
+    lastname: "",
+    firstname: "",
+    email: "",
+    content: "",
+  }
+
+  onSubmit =(): void => {
+    console.log(this.newMessage)
+    alert('Formulaire bien envoyé ! Merci '+this.newMessage.firstname)
+  }
+
+}
